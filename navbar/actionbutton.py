c024588bfbadc5a6b450b05a249e74a03f6a954a
cafehaine <kilian.guillaume@gmail.com>
2020-02-12 21:04:18 +0100

    Navbar: call long_press automatically after timeout

--- navbar/actionbutton.py
+++ navbar/actionbutton.py
@@ -1,3 +1,4 @@
+import asyncio
 from time import time
 from typing import Callable, List, Union
 
@@ -18,6 +19,7 @@ class ActionButton(Gtk.Button):
         self.long_press = long_press
         self.button_down = False
         self.button_down_timestamp = None
+        self.button_timer_handle = None
         self.connect('button-press-event', self.press_event)
         self.connect('button-release-event', self.release_event)
         self.connect('leave-notify-event', self.leave_event)
@@ -27,14 +29,17 @@ class ActionButton(Gtk.Button):
         if event.get_button()[1] == 1:
             self.button_down = True
             self.button_down_timestamp = time()
-            #TODO run callback in the future with asyncio to call
-            # call_long_press after LONG_PRESS_TIME even if click was not
-            # released
+
+            event_loop = asyncio.get_event_loop()
+            self.button_timer_handle = event_loop.call_later(LONG_PRESS_TIME, self.timer_event)
         else:
             self.button_down = False
 
     def release_event(self, button: 'ActionButton', event: Gdk.Event):
         """ On button release, call press or long_press if appropriate. """
+        if self.button_timer_handle is not None:
+            self.button_timer_handle.cancel()
+
         if event.get_button()[1] == 1 and self.button_down:
             if time() - self.button_down_timestamp >= LONG_PRESS_TIME:
                 self.call_long_press()
@@ -46,6 +51,11 @@ class ActionButton(Gtk.Button):
         """ On cursor leave, stop considering button is down. """
         self.button_down = False
 
+    def timer_event(self):
+        """ Call long_press and reset button_down after timer is ellapsed. """
+        self.call_long_press()
+        self.button_down = False
+
     def call_press(self) -> None:
         """ Execute the press action. """
         if isinstance(self.press, str):
@@ -54,6 +64,7 @@ class ActionButton(Gtk.Button):
         elif isinstance(self.press, list):
             func = try_get_action(self.press[0])
             func(*self.press[1:])
+        print(func.__name__)
 
     def call_long_press(self) -> None:
         """ Execute the long_press action. """
@@ -63,3 +74,4 @@ class ActionButton(Gtk.Button):
         elif isinstance(self.press, list):
             func = try_get_action(self.long_press[0])
             func(*self.long_press[1:])
+        print(func.__name__)
