ba93e879dd09e186fa3c48ce375ef4ac1bc11e28
cafehaine <kilian.guillaume@gmail.com>
2020-02-12 20:47:57 +0100

    Navbar: start implementing long_press on buttons

--- navbar/actionbutton.py
+++ navbar/actionbutton.py
@@ -1,10 +1,13 @@
+from time import time
 from typing import Callable, List, Union
 
 import gi
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk
+from gi.repository import Gtk, Gdk
 
 from .action import try_get_action
+
+LONG_PRESS_TIME = 0.8
 
 ConfigAction = Union[str, List]
 
@@ -13,16 +16,50 @@ class ActionButton(Gtk.Button):
         super().__init__(label=icon)
         self.press = press
         self.long_press = long_press
-        self.connect('clicked', self.call_action)
+        self.button_down = False
+        self.button_down_timestamp = None
+        self.connect('button-press-event', self.press_event)
+        self.connect('button-release-event', self.release_event)
+        self.connect('leave-notify-event', self.leave_event)
 
-    def call_action(self, button: Gtk.Button) -> None:
-        #TODO call press or long_press depending on click duration
-        self.call_press()
+    def press_event(self, button: 'ActionButton', event: Gdk.Event):
+        """ On button press, if button is 1, mark time stamp. """
+        if event.get_button()[1] == 1:
+            self.button_down = True
+            self.button_down_timestamp = time()
+            #TODO run callback in the future with asyncio to call
+            # call_long_press after LONG_PRESS_TIME even if click was not
+            # released
+        else:
+            self.button_down = False
+
+    def release_event(self, button: 'ActionButton', event: Gdk.Event):
+        """ On button release, call press or long_press if appropriate. """
+        if event.get_button()[1] == 1 and self.button_down:
+            if time() - self.button_down_timestamp >= LONG_PRESS_TIME:
+                self.call_long_press()
+            else:
+                self.call_press()
+        self.button_down = False
+
+    def leave_event(self, button: 'ActionButton', event: Gdk.Event):
+        """ On cursor leave, stop considering button is down. """
+        self.button_down = False
 
     def call_press(self) -> None:
+        """ Execute the press action. """
         if isinstance(self.press, str):
             func = try_get_action(self.press)
             func()
         elif isinstance(self.press, list):
             func = try_get_action(self.press[0])
             func(*self.press[1:])
+
+    def call_long_press(self) -> None:
+        """ Execute the long_press action. """
+        if isinstance(self.press, str):
+            func = try_get_action(self.long_press)
+            func()
+        elif isinstance(self.press, list):
+            func = try_get_action(self.long_press[0])
+            func(*self.long_press[1:])
